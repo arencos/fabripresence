8d3cb88eb4dbbb9e3610719a1cfe9c2f1c8c7c32
Aren <ar3n1596@gmail.com>
2021-04-16 11:07:05 +0300

    1.2.0

--- togit.bat
+++ togit.bat
@@ -1,7 +1,6 @@
-echo "# fabripresence" >> README.md
 git init
 git add .
-git commit -m "first commit"
+git commit -m "1.2.0"
+git branch -M main
 git remote add origin https://github.com/arencos/fabripresence.git
 git push -u origin main
-pause
